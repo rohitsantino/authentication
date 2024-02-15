41019976599458469807a736d2b0ba579aa3fd19
Rohit Singh <rohit.s@antino@antino.com>
2024-02-15 16:42:02 +0530

    feat:added fucntionality to login route and created auth middleware

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -7,16 +7,37 @@ const generateAccessAndRefershToken = async (userID) => {
     const user = await User.findById(userID);
     const accessToken = await user.generateAccessToken();
     const refreshToken = await user.genarteRefreshToken();
-    user.refreshToken=refreshToken;
+    user.refreshToken = refreshToken;
     await user.save({ validateBeforeSave: false });
     return { accessToken, refreshToken }
 }
 
-const login = asyncHandler(async (req, res) => { });
+const login = asyncHandler(async (req, res) => {
+    const{email,password}=req.body;
+    console.log(req.body);
+    if([email,password].some(field=>!field.trim())){
+        throw new ApiError(404,'All fields are required');
+    }
+    const existingUser=await User.findOne({email});
+    if(!existingUser){
+        throw new ApiError(400,"No such user exists!");
+    }
+    const isPasswordMatching=await existingUser.isPasswordCorrect(password);
+    console.log(isPasswordMatching);
+    if(!isPasswordMatching){
+        throw new ApiError(400,"Password does not match!")
+    }
+    const loggedInUser=await User.findById(existingUser._id).select("-password -refreshToken");
+    const {accessToken,refreshToken}=await generateAccessAndRefershToken(existingUser._id);
+    res.status(200).cookie("accessToken",accessToken,OPTIONS).cookie("refreshToken",refreshToken,OPTIONS).json(loggedInUser);
+ });
 
 const register = asyncHandler(async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
-    console.log(firstName,lastName,email,password,"check");
+    if ([firstName, lastName, email, password].some(field => !field.trim())) {
+        throw new ApiError(404, 'All fields are required');
+    }
+    console.log(firstName, lastName, email, password, "check");
     const existingUser = await User.findOne({ email });
     console.log(existingUser);
     if (existingUser) {
---
+++ server/src/middlewares/auth.middleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('../utils/asyncHandler');
+const ApiError = require('../utils/ApiError');
+const User = require('../models/user.model');
+
+
+const verifyJWT = asyncHandler(async (req, res, next) => {
+    const incomingAccessToken = req.cookies['accessToken'] || req.header('Authorization')?.replace('Bearer', "");
+    if (!incomingAccessToken) {
+        throw new ApiError(401, "Unauthorised request!");
+    }
+    const decodedToken = await jwt.verify(incomingAccessToken, process.env.ACCESS_TOKEN);
+    const user = await User.findById(decodedToken._id);
+    if (!user) {
+        throw new ApiError(401, "Invalid Access Token");
+    }
+    req.user=user;
+    next();
+})
+
+module.exports={verifyJWT};
